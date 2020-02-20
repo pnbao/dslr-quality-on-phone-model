d8e91453d5b22c67da47c19b1f50370ea4cb959e
pnbao <pnbao@acworks.co.jp>
2020-02-20 14:46:44 +0900

    change struture

--- run_model.py
+++ run_model.py
@@ -22,32 +22,32 @@ phone, resolution, use_gpu = utils.process_command_args(sys.argv)
 # disable gpu if specified
 config = tf.ConfigProto(device_count={'GPU': 0}) if use_gpu == "false" else None
 
-with tf.compat.v1.Session(config=config) as sess:
-    # load pre-trained model
-    saver = tf.compat.v1.train.Saver ()
-    saver.restore(sess, "models/" + phone)
+test_dir = "test_photos/" + phone + "/"
+test_photos = [f for f in os.listdir(test_dir) if os.path.isfile(test_dir + f)]
 
-    test_dir = "test_photos/" + phone + "/"
-    test_photos = [f for f in os.listdir(test_dir) if os.path.isfile(test_dir + f)]
+for photo in test_photos:
+    # get all available image resolutions
+    res_sizes = utils.get_resolutions(test_dir + photo)
 
-    for photo in test_photos:
+    # get the specified image resolution
+    IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_SIZE = utils.get_specified_res(res_sizes, phone, resolution)
 
-        # get all available image resolutions
-        res_sizes = utils.get_resolutions(test_dir + photo)
+    # create placeholders for input images
+    x_ = tf.compat.v1.placeholder(tf.float32, [None, IMAGE_SIZE])
+    x_image = tf.reshape(x_, [-1, IMAGE_HEIGHT, IMAGE_WIDTH, 3])
 
-        # get the specified image resolution
-        IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_SIZE = utils.get_specified_res(res_sizes, phone, resolution)
-
-        # create placeholders for input images
-        x_ = tf.compat.v1.placeholder(tf.float32, [None, IMAGE_SIZE])
-        x_image = tf.reshape(x_, [-1, IMAGE_HEIGHT, IMAGE_WIDTH, 3])
-
-        # generate enhanced image
-        enhanced = resnet(x_image)
+    # generate enhanced image
+    enhanced = resnet(x_image)
+    
+    with tf.compat.v1.Session(config=config) as sess:
+        # load pre-trained model
+        saver = tf.compat.v1.train.Saver ()
+        saver.restore(sess, "models/" + phone)
 
         # load training image and crop it if necessary
         print("Processing image " + photo)
         image = np.float16(np.array(Image.fromarray(imageio.imread(test_dir + photo)).resize(tuple(res_sizes[phone][::-1])))) / 255 
+            
         # image = np.float16(np.array(Image.fromarray(misc.imread(test_dir + photo)).resize(res_sizes[phone]))) / 255 1536, 2048
         image_crop = utils.extract_crop(image, resolution, phone, res_sizes)
         image_crop_2d = np.reshape(image_crop, [1, IMAGE_SIZE])
